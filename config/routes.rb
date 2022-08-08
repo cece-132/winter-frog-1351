c83e8c4cfc0fc5d9bd6a480ec4e23461f46cca27
Candace Eckels <candace.eckels@gmail.com>
2022-08-08 10:22:36 -0600

    US2 plot_plants_controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   resources :plots, only: [:index]
+  resources :plot_plants, only: [:update]
+
+  resources :gardens do
+    resources :plots, only: [:index]
+  end
 end
--- spec/features/plots/index_spec.rb
+++ spec/features/plots/index_spec.rb
@@ -22,7 +22,7 @@ RSpec.describe Plot do
       pp5 = PlotPlant.create!(plant_id: plant4.id, plot_id: plot2.id)
       pp6 = PlotPlant.create!(plant_id: plant5.id, plot_id: plot1.id)
 
-      visit plots_path
+      visit garden_plots_path(garden)
 
       within(".plot-#{plot1.id}") do
         expect(page).to have_content(plot1.number)
@@ -35,6 +35,46 @@ RSpec.describe Plot do
         expect(page).to_not have_content(plant3.name)
         expect(page).to_not have_content(plant4.name)
       end
+    end
+  end
+
+  describe 'User Story 2' do
+    it 'can remove a plant from A plot Index page' do
+      garden = Garden.create!(name: "Turing Community Garden", organic: true)
+
+      plot1 = Plot.create!(number: 25, size: "Large", direction: "East", garden_id: garden.id)
+      plot2 = Plot.create!(number: 35, size: "Small", direction: "South", garden_id: garden.id)
+      plot3 = Plot.create!(number: 45, size: "Medium", direction: "South", garden_id: garden.id)
+
+      plant1 = Plant.create!(name: "Purple Beauty Sweet Bell Pepper", description: "Prefers rich, well draining soil.", days_to_harvest: 90)
+      plant2 = Plant.create!(name: "Balloon Flower", description: "Great for the days you just want to float away", days_to_harvest: 45)
+      plant3 = Plant.create!(name: "Sunflower", description: "These seeds are the best for baseball games", days_to_harvest: 75)
+      plant4 = Plant.create!(name: "Wild asparagus", description: "Your family will love these", days_to_harvest: 90)
+      plant5 = Plant.create!(name: "Jerusalem artichoke", description: "Amazing dips have been made from this plant", days_to_harvest: 52)
+
+      pp1 = PlotPlant.create!(plant_id: plant1.id, plot_id: plot1.id)
+      pp2 = PlotPlant.create!(plant_id: plant1.id, plot_id: plot2.id)
+      pp3 = PlotPlant.create!(plant_id: plant2.id, plot_id: plot3.id)
+      pp4 = PlotPlant.create!(plant_id: plant3.id, plot_id: plot3.id)
+      pp5 = PlotPlant.create!(plant_id: plant4.id, plot_id: plot2.id)
+      pp6 = PlotPlant.create!(plant_id: plant5.id, plot_id: plot1.id)
+
+      visit garden_plots_path(garden)
+
+      within(".plot-#{plot1.id}") do
+        within(".plant-#{plant1.id}") do
+          expect(page).to have_content(plant1.name)
+          expect(page).to_not have_content(plant2.name)
+
+          pp = PlotPlant.where(plot_id: plot1.id).first
+          expect(pp.plot_id).to eq(plot1.id)
+
+          expect(page).to have_button("Remove Plant")
+
+          click_button "Remove Plant"
+        end
+      end
+
     end
   end
 end
